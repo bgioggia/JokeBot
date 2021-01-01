df4582654f09dcc8251ef5cc7a1e63370e790fce
Brandon Gioggia <gioggia.b@husky.neu.edu>
2021-01-01 01:22:30 -0500

    Added some api function descriptions.

--- api.py
+++ api.py
@@ -1,16 +1,19 @@
 import urllib
-from enum import Enum
 import requests
 from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup
 
 
 """
+This function will return true if the word has a definition that matches
+the given part of speech. If it does not, the function will return False.
+
 string -> Boolean
 """
 def verify_part_of_speech(word, posp):
     url = 'https://www.merriam-webster.com/dictionary/' + word
 
+    # error Handling if word does not exist in webster
     try:
         r = requests.get(url)
         r.raise_for_status()
@@ -18,6 +21,7 @@ def verify_part_of_speech(word, posp):
         print("Word not in Webster")
         return False
 
+    # look through webster html to confirm the given posp matches that of the word
     u = urllib.request.urlopen(url)
     soup = BeautifulSoup(u, 'html.parser')
     parts_of_speech = soup.findAll('a', {'class': 'important-blue-link'})
@@ -28,6 +32,8 @@ def verify_part_of_speech(word, posp):
 
 
 """
+This function will put any word into its singular present tense. 
+
 string -> string
 """
 def singularize_word(word):
@@ -47,6 +53,8 @@ def singularize_word(word):
 
 
 """
+Gets a word using the random word api
+
 void -> string
 """
 def get_word():
