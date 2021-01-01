3b077a0f75b07c41685db9ef790991a32ff12ff1
Brandon Gioggia <gioggia.b@husky.neu.edu>
2020-12-31 22:25:12 -0500

    Created functions to get a random word, verify that a word is a noun, and put any word into its singular form.

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
+import time
 import urllib
 import requests
 from bs4 import BeautifulSoup
 
 
+"""
+string -> Boolean
+"""
+def verify_noun(word):
+    url = 'https://www.merriam-webster.com/dictionary/' + word
+    u = urllib.request.urlopen(url)
+    soup = BeautifulSoup(u, 'html.parser')
+    parts_of_speech = soup.findAll('a', {'class': 'important-blue-link'})
+    for part in parts_of_speech:
+        if 'noun' in part:
+            return True
+    return False
 
 
+"""
+string -> string
+"""
+def singularize_word(word):
+    url = 'https://www.merriam-webster.com/dictionary/' + word
+    u = urllib.request.urlopen(url)
+    soup = BeautifulSoup(u, 'html.parser')
+    singular = str(soup.findAll('h1', {'class': 'hword'})[0])
+    singularized_word = ''
+    reached_word = False
+    for char in singular:
+        if char is '<' and reached_word:
+            return singularized_word
+        if reached_word:
+            singularized_word = singularized_word + char
+        if char == '>':
+            reached_word = True
 
-# Press the green button in the gutter to run the script.
+
+"""
+void -> string
+"""
+def get_word():
+    r = requests.get('https://random-word-api.herokuapp.com/word?number=1')
+    wrd = r.text
+    wrd = wrd[2:(len(wrd)-2)]
+    return wrd
+
+
+"""
+main
+"""
 if __name__ == '__main__':
-    u2 = urllib.request.urlopen('https://www.merriam-webster.com/dictionary/run')
-    soup = BeautifulSoup(u2, 'html.parser')
-    r = requests.get('https://random-word-api.herokuapp.com/word?number=10')
-    print(r.text)
-    #print(soup.prettify())container
-    mydivs = soup.findAll("a", {"class": "important-blue-link"})
-    m2ydivs = soup.findAll("div", {"class": "container"})
-    print(mydivs)
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    word = get_word()
+    print(word)
+    noun = verify_noun(word)
+    print(noun)
+    if noun:
+        time.sleep(.2)
+        print(singularize_word(word))
