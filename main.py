36f2e42868bf2738b690f7f2e1caf76705f67abb
Brandon Gioggia <gioggia.b@husky.neu.edu>
2021-01-01 15:16:50 -0500

    All main functions complete! App should be all set and ready to start adding discord integration.

--- main.py
+++ main.py
@@ -3,17 +3,55 @@ import words
 import jokes
 import random
 
+"""
 
-# this distribution of parts of speech should give all jokes roughly equal probability.
-parts_of_speech = ['noun', 'noun', 'noun', 'noun', 'noun', 'adjective', 'adjective']  # , 'verb']
+string, string -> string
+"""
+def prompt_from_word(word, part):
+    if part == 'noun':
+        jokes_list = [jokes.worlds_worst(word), jokes.back_in_my_day(word), jokes.sex_with_me(word),
+                      jokes.bar_joke(word), jokes.pickup_lines(word)]
+        return random.choice(jokes_list)
+    elif part == 'adjective':
+        jokes_list = [jokes.back_in_my_day(word), jokes.yo_mama(word)]
+        return random.choice(jokes_list)
+
+
+
+"""
+void -> string
+"""
+def prompt():
+    # this distribution of parts of speech should give all jokes roughly equal probability.
+    parts_of_speech = ['noun', 'noun', 'noun', 'noun', 'noun', 'adjective', 'adjective']  # , 'verb']
+    is_part = False
+    word = ''
+    part = random.choice(parts_of_speech)
+
+    #print(part)
+
+    while not is_part:
+        word = words.get_word()
+        is_part = words.verify_part_of_speech(word, part)
+        time.sleep(.35)
+
+        # print(word)
+        # print(is_part)
+
+    joke = prompt_from_word(word, part)
+    return joke
+    #print(api.singularize_word(word))
+
 
 """
 main
 """
 if __name__ == '__main__':
-    print()
-    print(jokes.back_in_my_day("apple"))
-    print(jokes.back_in_my_day("pear"))
+    print(prompt())
+
+
+
+
     """is_part = False
     word = ''
     part = random.choice(parts_of_speech)
